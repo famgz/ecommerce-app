15b6dc3cc9141844c9a5cd0650b58f0baaf83b56
famgz <famgz@proton.me>
2024-01-27 12:01:15 -0300

    feat: base home page structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import Nav from '@/components/Nav';
+import LoginWithGoogle from '@/components/buttons/LoginWithGoogle';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return (
+      <div className='flex items-center'>
+        <div className='text-center w-full'>
+          <LoginWithGoogle />
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="bg-red-500">
-      ecommerce
+    <div>
+      <Nav />
+      <div>logged in: {session?.user?.email}</div>
     </div>
-  )
+  );
 }
