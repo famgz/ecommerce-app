23095590825b29c114f1e8391104f9918d8f9e3b
famgz <famgz@proton.me>
2024-01-27 12:23:34 -0300

    refactor: global layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,9 @@
 import { Roboto } from 'next/font/google';
 import './globals.css';
+import Nav from '@/components/Nav';
+import LoginWithGoogle from '@/components/buttons/LoginWithGoogle';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -11,13 +15,28 @@ export const metadata = {
   description: 'E-Commerce App created with NextJS',
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return (
+      <div className='flex items-center'>
+        <div className='text-center w-full'>
+          <LoginWithGoogle />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <html lang='en'>
       <body className={roboto.className}>
-        <main className='bg-blue-900 text-white w-screen h-screen'>
-          {children}
-        </main>
+        <div className='bg-blue-900 text-white flex min-h-screen'>
+          <Nav />
+          <main className='bg-white text-black flex-grow m-2 ml-0 rounded-lg p-4'>
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,25 +1,7 @@
-import Nav from '@/components/Nav';
-import LoginWithGoogle from '@/components/buttons/LoginWithGoogle';
-import { getServerSession } from 'next-auth';
-import { authOptions } from './api/auth/[...nextauth]/route';
-
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return (
-      <div className='flex items-center'>
-        <div className='text-center w-full'>
-          <LoginWithGoogle />
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <div className='flex min-h-screen'>
-      <Nav />
-      <div className='bg-white text-black flex-grow m-2 ml-0 rounded-lg p-4'>logged in: {session?.user?.email}</div>
+    <div className=''>
+      test
     </div>
   );
 }
