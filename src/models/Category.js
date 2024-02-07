609c777bf337d1c43a09c3e55c8e49ea0648dab1
famgz <famgz@proton.me>
2024-02-07 11:52:56 -0300

    feat: add DB's category parent field

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,7 +1,8 @@
-const { Schema, models, model } = require('mongoose');
+const { Schema, models, model, default: mongoose } = require('mongoose');
 
 const CategorySchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true, trim: true, minLength: 3, maxLength: 30 },
+  parent: {type: mongoose.Types.ObjectId, ref: 'Category', default: null}
 });
 
 export const Category = models?.Category || model('Category', CategorySchema);
