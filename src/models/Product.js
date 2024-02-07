fe5c41ea0d399456e7f2cfe119116d6e4a9ae0a8
famgz <famgz@proton.me>
2024-02-07 13:51:20 -0300

    chore: add category and timestamps to Product model

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,10 +1,11 @@
-const { Schema, models, model } = require('mongoose');
+const { Schema, models, model, default: mongoose } = require('mongoose');
 
 const ProductSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  images: {type: [String], default: []}
+  images: {type: [String], default: []},
+  category: {type: mongoose.Types.ObjectId, ref: 'Category', default: null},
 }, {timestamps: true});
 
 export const Product = models?.Product || model('Product', ProductSchema);
