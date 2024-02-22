9ac2135229f896f97baa7b4c611a74d8223d3135
famgz <famgz@proton.me>
2024-02-22 14:34:22 -0300

    refactor: cart totals moved out of table component. minor style fixes

--- src/app/(front)/_components/CartProductsTable.jsx
+++ src/app/(front)/_components/CartProductsTable.jsx
@@ -2,13 +2,8 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 
-export default function CartProductsTable({ products, cartContext }) {
+export default function CartProductsTable({ products, cartContext, total }) {
   const { addProduct, removeProduct, removeAllOfProductId } = cartContext;
-
-  let total = 0;
-  products.forEach((p) => {
-    total += p.price * p.quantity;
-  });
 
   const quantityButtonStyle =
     'inline-flex items-center justify-center text-center bg-gray-400 rounded-full size-5 text-white font-bold';
@@ -25,92 +20,70 @@ export default function CartProductsTable({ products, cartContext }) {
   }
 
   return (
-    <div>
-      <table className='table w-full border-spacing-y-3'>
-        <thead>
-          <tr className='text-gray-700'>
-            <th className='text-left'>PRODUCT</th>
-            <th>PRICE</th>
-            <th>QUANTITY</th>
-            <th>TOTAL</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody className='text-center'>
-          {products.map((p, i) => (
-            <tr key={i}>
-              {/* TITLE AND IMAGE */}
-              <td className='!pl-0'>
-                <span className='block font-bold text-left'>{p.title}</span>
-                <div className='relative size-[84px] md:size-[128px] mr-auto rounded-md border'>
-                  <Image
-                    src={p.images[0]}
-                    alt={p.description}
-                    fill
-                    className='object-contain p-4'
-                  />
-                </div>
-              </td>
-
-              {/* PRICE */}
-              <td>
-                <span className='font-medium text-lg'>R${p.price}</span>
-              </td>
-
-              {/* QUANTITY */}
-              <td>
-                <div className='flex items-center justify-center'>
-                  {/* quantity - */}
-                  <button
-                    className={quantityButtonStyle}
-                    onClick={() => decreaseProductQuantity(p._id, p.quantity)}
-                  >
-                    -
-                  </button>
-                  {/* quantity value */}
-                  <span className='py-2 px-4 mx-2 border rounded-md font-bold text-gray-600 text-lg'>
-                    {p.quantity}
-                  </span>
-                  {/* quantity + */}
-                  <button
-                    className={quantityButtonStyle}
-                    onClick={() => increaseProductQuantity(p._id)}
-                  >
-                    +
-                  </button>
-                </div>
-              </td>
-
-              {/* TOTAL PRICE */}
-              <td>
-                <span className='font-bold text-xl '>
-                  R${p.price * p.quantity}
-                </span>
-              </td>
-
-              {/* DELETE BUTTON */}
-              <td className='!pr-0'>
-                <button
-                  className='w-full p-2 text-right'
-                  onClick={() => removeAllOfProductId(p._id)}
-                >
-                  <FontAwesomeIcon
-                    icon={faTrash}
-                    className='size-4 text-gray-500'
-                  />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-
-        {/* TOTALS */}
-        <tfoot>
-          <tr>
-            <th className='text-left text-xl'>TOTAL</th>
-            <td colSpan={3} className='font-bold text-xl text-right'>
-              R${total}
+    // <div>
+    <table className='table w-full border-spacing-y-3'>
+      <thead>
+        <tr className='text-gray-700'>
+          <th className='text-left !pl-0'>PRODUCT</th>
+          <th className=''>PRICE</th>
+          <th>QUANTITY</th>
+          <th>TOTAL</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody className='text-center'>
+        {products.map((p, i) => (
+          <tr key={i}>
+            {/* TITLE AND IMAGE */}
+            <td className='!pl-0'>
+              <span className='block font-bold text-left'>{p.title}</span>
+              <div className='relative size-[84px] md:size-[128px] mr-auto rounded-md border'>
+                <Image
+                  src={p.images[0]}
+                  alt={p.description}
+                  fill
+                  className='object-contain p-4'
+                />
+              </div>
             </td>
+
+            {/* PRICE */}
+            <td>
+              <span className='font-medium text-lg'>R${p.price}</span>
+            </td>
+
+            {/* QUANTITY */}
+            <td>
+              <div className='flex items-center justify-center'>
+                {/* quantity - */}
+                <button
+                  className={quantityButtonStyle}
+                  onClick={() => decreaseProductQuantity(p._id, p.quantity)}
+                >
+                  -
+                </button>
+                {/* quantity value */}
+                <span className='py-1 px-3 mx-2 border rounded-md font-bold text-gray-600 text-lg'>
+                  {p.quantity}
+                </span>
+                {/* quantity + */}
+                <button
+                  className={quantityButtonStyle}
+                  onClick={() => increaseProductQuantity(p._id)}
+                >
+                  +
+                </button>
+              </div>
+            </td>
+
+            {/* TOTAL PRICE */}
+            <td>
+              <span className='font-bold text-xl '>
+                R${p.price * p.quantity}
+              </span>
+            </td>
+
+            {/* DELETE BUTTON */}
             <td className='!pr-0'>
               <button
                 className='w-full p-2 text-right'
@@ -123,9 +96,31 @@ export default function CartProductsTable({ products, cartContext }) {
               </button>
             </td>
           </tr>
-        </tfoot>
-      </table>
-    </div>
+        ))}
+      </tbody>
+
+      {/* TOTALS */}
+      <tfoot>
+        <tr>
+          <th className='text-left text-xl'>TOTAL</th>
+          <td colSpan={3} className='font-bold text-xl text-right'>
+            R${total}
+          </td>
+          <td className='!pr-0'>
+            <button
+              className='w-full p-2 text-right'
+              onClick={() => removeAllOfProductId(p._id)}
+            >
+              <FontAwesomeIcon
+                icon={faTrash}
+                className='size-4 text-gray-500'
+              />
+            </button>
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+    // </div>
   );
 }
 // }
