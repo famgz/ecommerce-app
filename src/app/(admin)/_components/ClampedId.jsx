cca7d40e280f3321bb00d2d7451ac51acb1ff7d1
famgz <famgz@proton.me>
2024-02-28 15:20:54 -0300

    refactor: change clampedId alignment

--- src/app/(admin)/_components/ClampedId.jsx
+++ src/app/(admin)/_components/ClampedId.jsx
@@ -3,9 +3,11 @@ import { faCopy } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function ClampedId({ _id }) {
+  const id = clampId(_id);
+
   return (
-    <span title={_id}>
-      {clampId(_id)}
+    <div title={_id} className='w-fit flex items-center'>
+      {id}
       <div
         onClick={() => copyToClipboard(_id)}
         className='text-center p-1 cursor-pointer'
@@ -13,6 +15,6 @@ export default function ClampedId({ _id }) {
       >
         <FontAwesomeIcon icon={faCopy} className='size-4 text-gray-500' />
       </div>
-    </span>
+    </div>
   );
 }
