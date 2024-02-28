6288e58e8edc134ec0bc2fec09cfb9286ea7eb1f
famgz <famgz@proton.me>
2024-02-28 16:48:41 -0300

    feat: payment status integration and validation

---
+++ src/app/api/webhook/route.js
@@ -0,0 +1,41 @@
+import { mongooseConnect } from '@/app/(admin)/_libs/mongoose';
+import { Order } from '@/app/(admin)/_models/Order';
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export async function POST(req) {
+  await mongooseConnect();
+
+  const sig = req.headers.get('stripe-signature');
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      await req.text(),
+      sig,
+      process.env.STRIPE_SIGN_SECRET
+    );
+  } catch (err) {
+    console.error(`Webhook Error: ${err.message}`);
+    return Response.json(err, { status: 400 });
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.completed':
+      const eventData = event.data.object;
+      const paid = eventData.payment_status === 'paid';
+      const orderId = eventData?.metadata?.orderId;
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId, { paid: true });
+        console.log('Order succesfully paid!');
+      }
+
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  return Response.json('ok', { status: 200 });
+}
