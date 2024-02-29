8c78252af37b2825c4564658369eef4bced8c692
famgz <famgz@proton.me>
2024-02-29 16:32:58 -0300

    refactor: move getAllProducts to productUtils file

--- src/app/(front)/page.jsx
+++ src/app/(front)/page.jsx
@@ -1,17 +1,9 @@
-import { mongooseConnect } from '@/app/_libs/mongoose';
-import { Product } from '@/app/_models/Product';
 import FeaturedProduct from '@/app/(front)/_components/FeaturedProduct';
 import NewProducts from '@/app/(front)/_components/NewProducts';
-import { convertToPlainObject } from '@/app/_libs/utils';
-
-export async function getAllProducts() {
-  await mongooseConnect();
-  return await Product.find({}, null, { sort: { updatedAt: -1 } });
-}
+import { getProducts } from '@/app/_libs/productsUtils';
 
 export default async function HomePage() {
-  const rawProducts = await getAllProducts();
-  const allProducts = convertToPlainObject(rawProducts);
+  const allProducts = await getProducts(8);
   const featuredProduct = allProducts.find(
     (p) => p._id === '65d2425410dec5df9236f3c6'
   );
