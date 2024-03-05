15ec27404bf3b9f46ec84922eeca2483e9535b2a
famgz <famgz@proton.me>
2024-03-05 12:42:29 -0300

    fix: FeatureProduct component responsitivy

--- src/app/(front)/_components/FeaturedProduct.jsx
+++ src/app/(front)/_components/FeaturedProduct.jsx
@@ -5,26 +5,36 @@ import Link from 'next/link';
 export default function FeaturedProduct({ product }) {
   return (
     <div className='bg-zinc-800 text-white py-12'>
-      <div className='_container grid grid-cols-[.8fr_1.2fr] gap-2'>
-        <div className='flex flex-col gap-5 justify-center'>
-          <h1 className='text-5xl font-medium text-left'>{product.title}</h1>
-          <p className='text-sm text-gray-500 font-semibold'>
-            {product.description}
-          </p>
-          <div className='flex gap-2'>
-            <Link href='/' className='btn-trans '>
-              Read more
-            </Link>
-            <AddToCartButton product={product} />
+      <div className='_container gap-2'>
+        <h1 className='!text-5xl !font-normal text-center sm:text-left'>
+          {product.title}
+        </h1>
+        <div className='flex flex-col-reverse sm:grid sm:grid-cols-[.8fr_1.2fr] mt-3'>
+          <div className='flex flex-col gap-5 justify-center'>
+            <p className='text-sm text-gray-500 font-semibold'>
+              {product.description}
+            </p>
+            <div className='flex flex-col sm:flex-row gap-2'>
+              <Link
+                href={'/products/' + product._id}
+                className='btn-trans whitespace-nowrap'
+              >
+                Read more
+              </Link>
+              <AddToCartButton
+                product={product}
+                className='whitespace-nowrap'
+              />
+            </div>
           </div>
-        </div>
-        <div className='relative min-h-[300px]'>
-          <Image
-            src={product.images[0]}
-            alt='image'
-            fill
-            className='object-contain'
-          />
+          <div className='relative size-full min-w-[200px] min-h-[200px] sm:min-h-[300px]'>
+            <Image
+              src={product.images[0]}
+              alt='image'
+              fill
+              className='object-contain'
+            />
+          </div>
         </div>
       </div>
     </div>
